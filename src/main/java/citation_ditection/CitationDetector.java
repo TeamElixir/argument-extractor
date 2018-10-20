88d57221f513aa76ad7e24ff0bc62352f79b5ca2
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2018-10-20 20:53:27 +0530

    duplication is fixed, topic related issue is fixed

--- src/main/java/citation_ditection/CitationDetector.java
+++ src/main/java/citation_ditection/CitationDetector.java
@@ -18,6 +18,7 @@ public class CitationDetector {
     static ArrayList<String> prevCasePetitionerList = new ArrayList<>();
     static String prevSentence = "";
     static ArrayList<String> caseLawSentencesList = new ArrayList<>();
+    static String petitionerTopic = "";
 
     //this variable is used to solve Ibid, Id relevance to caselaw sentences
     static boolean isPreviousCitation = false;
@@ -36,7 +37,17 @@ public class CitationDetector {
 
         //to remove the four sentences contatining topic and other meta data
 
+        String supremecourtStmt = sc.nextLine();
+        while(supremecourtStmt.trim().length()==0){
+            supremecourtStmt = sc.nextLine();
+        }
+
         String topic = sc.nextLine();
+        while(topic.trim().length()==0){
+            topic = sc.nextLine();
+        }
+        petitionerTopic = petitionerTopic(topic).toLowerCase();
+        System.out.println(petitionerTopic);
         sc.nextLine();
         sc.nextLine();
         sc.nextLine();
@@ -45,21 +56,21 @@ public class CitationDetector {
             String paragraph = sc.nextLine();
             Annotation ann = nlpUtils.annotate(paragraph);
 
-            if(topic.trim().equals(paragraph.trim())){
+            if(sentenceSimilarity(paragraph.toLowerCase().trim(),petitionerTopic)){
                 continue;
             }
 
             List<CoreMap> sentences = ann.get(CoreAnnotations.SentencesAnnotation.class);
             for (CoreMap sentence : sentences) {
                 String line = completefiltering(sentence.toString());
+
+                if(!pre_filtering(sentence.toString())){
+                    continue;
+                }
                 if(line.length()==0){
-                    bw_non_arg.write(sentence.toString());
-                    bw_non_arg.newLine();
-                    bw_non_arg.flush();
+
                 }else{
-                    bw_arg.write(line);
-                    bw_arg.newLine();
-                    bw_arg.flush();
+                    filterDuplicates(line);
                 }
                 //keep this as the last line within for loop
                 prevSentence = sentence.toString();
@@ -69,14 +80,71 @@ public class CitationDetector {
             iscompleted = true;
         }
 
+        System.out.println();
+        for( String sentence:caseLawSentencesList){
+            bw_arg.write(sentence);
+            bw_arg.newLine();
+            bw_arg.flush();
+        }
+
+        for(String p: prevCasePetitionerList){
+            System.out.println(p);
+        }
+
         bw_arg.close();
         bw_non_arg.close();
+
+
+    }
+
+    public static String petitionerTopic(String text){
+        Pattern pattern = Pattern.compile("([A-Za-z]+\\s)+v\\.\\s[A-Za-z]+(\\s[A-Za-z])*");
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find())
+        {
+            String matchedText = matcher.group();
+            return matchedText.split("v.")[0].trim();
+        }
+        return "";
+    }
+
+    public static boolean sentenceSimilarity(String text1, String text2){
+        for(int i=0;i< Math.min(text1.length(),text2.length());i++){
+            if(text1.charAt(i)!=text2.charAt(i)){
+                if(i<10){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void filterDuplicates(String text){
+
+        if(caseLawSentencesList.size()>0 && text.contains(caseLawSentencesList.get(caseLawSentencesList.size()-1))){
+            caseLawSentencesList.remove(caseLawSentencesList.size()-1);
+            caseLawSentencesList.add(text);
+        }else{
+            caseLawSentencesList.add(text);
+        }
 
     }
 
     //Identifcation based on v. tag : ex: Hill v. Lockhart
     public static boolean v_identify(String text){
         String regex = "^.*[A-Za-z]+\\sv\\.\\s[A-Za-z]+.*$";
+
+        Pattern pattern = Pattern.compile("[A-Za-z]+\\sv\\.\\s[A-Za-z]+(\\s[A-Za-z])*[\\s|,][\\s][0-9]+\\sU.[\\s]S.");
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find())
+        {
+            String matchedText = matcher.group();
+            String petitioner = matchedText.split("v.")[0].trim();
+            if(!petitionerTopic.contains(petitioner) && petitioner.trim().length()>0 && !petitioner.toLowerCase().contains("states")){
+                System.out.println(text);
+                prevCasePetitionerList.add(petitioner);
+            }
+        }
 
         if(text.matches(regex)){
             //System.out.println(text);
@@ -112,14 +180,6 @@ public class CitationDetector {
     //Identication based on petitioner name : Pointing to Strickland, Government argues that...
     public static boolean petitioner_based_identify(String text){
         // this part is to identify petitioner names from previous cases
-        Pattern pattern = Pattern.compile("[A-Za-z]+\\sv\\.\\s[A-Za-z]+(\\s[A-Za-z])*[\\s|,][\\s][0-9]+\\sU.[\\s]S.");
-        Matcher matcher = pattern.matcher(text);
-        if (matcher.find())
-        {
-            String matchedText = matcher.group();
-            prevCasePetitionerList.add(matchedText.split("v.")[0].trim());
-        }
-
         for(String petitionerName : prevCasePetitionerList){
             if(text.contains(petitionerName)){
                 return true;
@@ -130,9 +190,9 @@ public class CitationDetector {
 
     //identication based on see. See Ins v. Car
     public static boolean see_identify(String text){
-        String regex = "^See\\s.*$";
+        String regex = "^See,*\\s.*$";
 
-        if(text.matches(regex) && !text.contains("See Brief")){
+        if(text.trim().matches(regex) && (text.contains("§") || v_identify(text) || case_number_identify(text) || petitioner_based_identify(text) || ibid_identify(text))){
             return true;
         }
         return false;
@@ -175,6 +235,16 @@ public class CitationDetector {
         return false;
     }
 
+    public static boolean pre_filtering(String text){
+        String regex1 = "^Opinion of the Court.*$";
+        String regex2 = "^No\\.\\s*[0-9]*-[0-9]*.*$";
+
+        if(text.trim().matches(regex1) || text.trim().matches(regex2)){
+            return false;
+        }
+        return true;
+    }
+
     //todo: is completed is added for later use: to avoid unecessary tags from a case
     public static String completefiltering(String text){
 
@@ -182,7 +252,9 @@ public class CitationDetector {
         if(iscompleted){
             if(see_identify(text)){
                 isPreviousCitation = true;
-                return prevSentence+" "+text;
+                if(alpha_words_based_filtering(prevSentence)){
+                    return prevSentence+" "+text;
+                }
             }
 
             if(!(v_identify(text) || case_number_identify(text) || petitioner_based_identify(text))){
@@ -191,19 +263,24 @@ public class CitationDetector {
             }
 
             if((v_identify(text) || case_number_identify(text) || petitioner_based_identify(text))
+                    && !alpha_words_based_filtering(text)){
+                isPreviousCitation = true;
+                if(alpha_words_based_filtering(prevSentence)){
+                    return prevSentence+" "+text;
+                }
+
+            }
+
+            if((v_identify(text) || case_number_identify(text) || petitioner_based_identify(text))
                     && alpha_words_based_filtering(text)){
                 isPreviousCitation = true;
                 return text;
             }
 
-            if((v_identify(text) || case_number_identify(text) || petitioner_based_identify(text))
-                    && !alpha_words_based_filtering(text)){
-                isPreviousCitation = true;
-                return prevSentence+" "+text;
-            }
-
             if(isPreviousCitation && ibid_identify(text)){
-                return prevSentence;
+                if(alpha_words_based_filtering(prevSentence)){
+                    return prevSentence;
+                }
             }
 
             if(app_identify(text)){
@@ -214,7 +291,9 @@ public class CitationDetector {
         else{
             if(see_identify(text)){
                 isPreviousCitation = true;
-                return prevSentence+" "+text;
+                if(alpha_words_based_filtering(prevSentence)){
+                    return prevSentence+" "+text;
+                }
             }
 
             if((v_identify(text) || case_number_identify(text) || petitioner_based_identify(text))
@@ -226,11 +305,15 @@ public class CitationDetector {
             if((v_identify(text) || case_number_identify(text) || petitioner_based_identify(text))
                     && !alpha_words_based_filtering(text)){
                 isPreviousCitation = true;
-                return prevSentence+" "+text;
+                if(alpha_words_based_filtering(prevSentence)){
+                    return prevSentence+" "+text;
+                }
             }
 
             if(isPreviousCitation && ibid_identify(text)){
-                return prevSentence+" "+text;
+                if(alpha_words_based_filtering(prevSentence)){
+                    return prevSentence+" "+text;
+                }
             }
 
             if(app_identify(text)){
